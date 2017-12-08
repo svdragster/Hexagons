eec72d232c31710f3ecd0130ca9c6d80984c9740
svdragster <svdragster@gmail.com>
2017-12-08 09:47:07 +0100

    Initial commit

---
+++ ios/src/de/svdragster/hexagons/IOSLauncher.java
@@ -0,0 +1,22 @@
+package de.svdragster.hexagons;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import de.svdragster.hexagons.Hexagons;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Hexagons(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
